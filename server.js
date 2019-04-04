161f334fc652305105535b4da8e6649f89ce464b
RoelVoordendag <rvoordendag@gmail.com>
2019-04-04 16:46:22 +0200

    added port config

--- server.js
+++ server.js
@@ -6,3 +6,5 @@ const port = process.env.PORT || 8000;
 const server = http.createServer(app);
 //let the server lissen to port 8000
 server.listen(port);
+
+console.log("We running on port " + port);
